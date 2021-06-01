575ff0f799e3f8dca9d9e3eafe8fe592eccbe4e8
unknown <jupobatero1@gmail.com>
2021-06-01 20:55:10 -0300

    commit anasheeeeeeeeeeeeeeeeeee

---
+++ ApiCovid.js
@@ -0,0 +1,9 @@
+class ApiCovid {
+
+    getCases() {
+        fetch("https://covid-api.mmediagroup.fr/v1/cases").then(response=>response.json()).then(json=>console.log(json)).catch(e=>console.error(e)); //codigo de german aka william wallace
+    }
+
+}
+
+export default ApiCovid;
---
+++ index.js
@@ -0,0 +1,5 @@
+import ApiCovid from "./ApiCovid.js";
+
+let covidicion = new ApiCovid();
+
+covidicion.getCases();
