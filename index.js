144aebfeabb83cb1e804edc3c8de07099631e05d
unknown <jupobatero1@gmail.com>
2021-06-16 20:55:56 -0300

    IAN POR POR FAVOR

--- index.js
+++ index.js
@@ -2,6 +2,4 @@ import ApiCovid from "./ApiCovid.js";
 import CovidTablePopulator from "./CovidTablePopulator.js";
 
 CovidTablePopulator.countries = await ApiCovid.getCountries();
-CovidTablePopulator.originalCountries = CovidTablePopulator.countries;
 CovidTablePopulator.setTableCountry();
-CovidTablePopulator.sortColumn();
