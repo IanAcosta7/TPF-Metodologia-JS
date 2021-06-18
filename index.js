1a691e9cd2a760f7ead74ba1b409e2fb5a252ec7
mennaenzo <menna.enzo@hotmail.com>
2021-06-18 01:11:35 -0300

    Data ApiCovi in Map

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 import ApiCovid from "./ApiCovid.js";
 import CovidTablePopulator from "./CovidTablePopulator.js";
+import Map from "./Map.js";
 
 CovidTablePopulator.countries = await ApiCovid.getCountries();
 CovidTablePopulator.setTableCountry();
+Map.loadMap(CovidTablePopulator.countries);
